246a1e4af4c916721a2d4350abd152bb27b2361f
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-17 16:11:11 +0530

    17-05-2023

--- REST API - CRUD/myapp/views.py
+++ REST API - CRUD/myapp/views.py
@@ -8,6 +8,6 @@ class BookList(generics.ListCreateAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 
-class BookDetail(generics.RetriveUpdateDestroyAPIView):
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Book
 	serializer_class = BookSerializer
--- REST API - CRUD/mysite/urls.py
+++ REST API - CRUD/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import BookList,BookDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/books',BookList.as_view()),
+    path('api/books/<int:pk>',BookDetail.as_view()),
 ]
