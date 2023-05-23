b26891ade07837aca45a9d0c461d64c7ff9129af
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-23 23:03:42 +0530

    23-05-2023

--- REST API Assessment/mysite/urls.py
+++ REST API Assessment/mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import TaskList,TaskDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/tasks',TaskList.as_view()),
+    path('api/tasks/<int:pk>',TaskDetail.as_view()),
 ]
